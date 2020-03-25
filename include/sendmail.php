6dcd683b067d62254b23901722ee6bbb9eec7c3d
Jaeyun Noh <jaeyun@gmail.com>
2020-03-24 23:30:02 -0700

    fixing sendmail

--- include/sendmail.php
+++ include/sendmail.php
@@ -10,36 +10,34 @@ $contact_email = 'contact@uicbiz.com';
 // type
 $type = $_REQUEST['type'];	
 // parse
-parse_str($_POST['data'], $post_data);	
+$user_name = stripslashes(strip_tags(trim($_POST['username'])));
+$user_email = stripslashes(strip_tags(trim($_POST['email'])));
+$user_subject = stripslashes(strip_tags(trim($_POST['subject'])));
+$phone = stripslashes(strip_tags(trim($_POST['phone'])));
+$user_msg =stripslashes(strip_tags(trim( $_POST['message'])));
+	
+if (trim($contact_email)!='') {
+	$subj = 'Message from Invetex';
+	$msg = $subj." \r\nName: $user_name \r\nE-mail: $user_email \r\nPhone: $phone \r\nSubject: $user_subject \r\nMessage: $user_msg";
 
-		$user_name = stripslashes(strip_tags(trim($post_data['username'])));
-		$user_email = stripslashes(strip_tags(trim($post_data['email'])));
-		$user_subject = stripslashes(strip_tags(trim($post_data['subject'])));
-		$phone = stripslashes(strip_tags(trim($post_data['phone'])));
-		$user_msg =stripslashes(strip_tags(trim( $post_data['message'])));
-			
-		if (trim($contact_email)!='') {
-			$subj = 'Message from Invetex';
-			$msg = $subj." \r\nName: $user_name \r\nE-mail: $user_email \r\nPhone: $phone \r\nSubject: $user_subject \r\nMessage: $user_msg";
+	$head = "Content-Type: text/plain; charset=\"utf-8\"\n"
+		. "X-Mailer: PHP/" . phpversion() . "\n"
+		. "Reply-To: $user_email\n"
+		. "To: $contact_email\n"
+		. "From: $user_email\n";
+		$errorMessage = "Message sent successfully!";
+	if (!mail($contact_email, $subj, $msg, $head)) {
+		$errorMessage = error_get_last()['message'];
+		$response['error'] = 'Error send message!';
 		
-			$head = "Content-Type: text/plain; charset=\"utf-8\"\n"
-				. "X-Mailer: PHP/" . phpversion() . "\n"
-				. "Reply-To: $user_email\n"
-				. "To: $contact_email\n"
-				. "From: $user_email\n";
-		        $errorMessage = "Message sent successfully!";
-			if (!mail($contact_email, $subj, $msg, $head)) {
-				$errorMessage = error_get_last()['message'];
-				$response['error'] = 'Error send message!';
-				
-			}
-		} else 
-				$errorMessage = error_get_last()['message'];
-				$response['error'] = 'Error send message!';	
-		
-		
+	}
+} else 
+		$errorMessage = error_get_last()['message'];
+		$response['error'] = 'Error send message!';	
 
-	// echo json_encode($post_data['username'].''.$post_data['email'].''$post_data['subject'].''.$post_data['message']);
+
+
+	// echo json_encode($_POST['username'].''.$_POST['email'].''$_POST['subject'].''.$_POST['message']);
 	// echo json_encode($response);
     echo $errorMessage
 	//die();
